109fba8f709412821fd1c178e1d2f19322ac362e
Minh Pham <minhp4801@gmail.com>
2018-10-21 07:55:42 -0400

    Update translate.py

--- translate.py
+++ translate.py
@@ -1,19 +1,14 @@
 # Imports the Google Cloud client library
 from google.cloud import translate
 
-# Instantiates a client
-translate_client = translate.Client()
+def translate_text(text, target):
+    # Instantiates a client
+    translate_client = translate.Client()
 
-# The text to translate
-text = u'Summarize'
-# The target language
-# Refer to language_ennumeration.txt for target language codes
-target = 'el'
+    # Translates some text into target language
+    translation = translate_client.translate(
+        text,
+        target_language=target)
 
-# Translates some text into target language
-translation = translate_client.translate(
-    text,
-    target_language=target)
-
-print(u'Text: {}'.format(text))
-print(u'Translation: {}'.format(translation['translatedText']))
+    print(u'Text: {}'.format(text))
+    print(u'Translation: {}'.format(translation['translatedText']))
