5b5afb826d128179fe6655b4fc27fcd75db55f39
Minh Pham <minhp4801@gmail.com>
2018-10-21 04:35:51 -0400

    Updated summary.py to include keyword functionality and added paratore.txt.

--- summary.py
+++ summary.py
@@ -1,10 +1,35 @@
 from gensim.summarization.summarizer import summarize
+from gensim.summarization import keywords
 
 def summary(text_file):
     try:
         file = open(text_file, "r")
-        text = file.read().replace('\n', ' ').replace('\r', ' ')
-        summary.summarized_text = summarize(text)
-        print(summary.summarized_text)
-    except (FileNotFoundError, IOError) as e:
+        text = file.read()
+        text = text.replace('\n', ' ').replace('\r', ' ')
+        output = open("output.txt", "w")
+        output.write(text)
+        print(text)
+        file.close()
+        output.close()
+    except (IOError, FileNotFoundError) as e:
+        print("Error in reading input file.")
         pass
+
+def get_keywords():
+    try:
+        summmary_file = open("output.txt", "r").read()
+        kw = keywords(summary_file)
+        output = open("keywords.txt", "w")
+        output.write(kw)
+        print(output)
+        summary_file.close()
+        output.close()
+    except (IOError, FileNotFoundError) as e:
+        print("Error in reading input file.")
+        pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="Uses gensim to summarize a given text file.")
+    parser.add_argument("-i","--input", help = "Name of plain text file containing our corpus.", required = True)
+    args = parser.parse_args()
+    summary(args.input)
